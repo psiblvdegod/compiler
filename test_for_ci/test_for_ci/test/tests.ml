3d7d0ba6a13c20c1304655352a0db840bf9475e7
psiblvdegod <artyombaly@list.ru>
2025-06-29 05:32:08 +0300

    add test that should fail

--- test_for_ci/test_for_ci/test/tests.ml
+++ test_for_ci/test_for_ci/test/tests.ml
@@ -3,6 +3,9 @@ open Alcotest
 let test_to_pass () =
   check bool "test_to_pass" (true) true
 
-let suite = [("test_to_pass",`Quick, test_to_pass);]
+let test_to_fail () =
+  check bool "test_to_pass" (true) false
 
-let () = run "tests" [("test_to_pass", suite);]
+let suite = [("test_to_pass",`Quick, test_to_pass);("test_to_fail",`Quick, test_to_fail);]
+
+let () = run "tests" [("tests", suite);]

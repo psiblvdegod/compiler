7e764889660a7b81d6a722c051b7a743084a8a4a
psiblvdegod <artyombaly@list.ru>
2025-06-30 17:02:38 +0300

    add simple version of lexer
    commit messages:
    
    add simple test for parser
    minor changes
    add multiplication to parser
    add some tests for parser

--- parser/parser/expression.ml
+++ parser/parser/expression.ml
@@ -1,5 +1,9 @@
 type expression =
   | Integer of int
   | Variable of string
+
   | Assignment of string * expression
+
   | Addition of expression * expression
+
+  | Multiplication of expression * expression

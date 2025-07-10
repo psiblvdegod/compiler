1c2d064b22d320c4abf7ed98875c6422f97320f7
psiblvdegod <artyombaly@list.ru>
2025-07-11 02:08:34 +0300

    minor style changes

--- lexer/processing.ml
+++ lexer/processing.ml
@@ -28,48 +28,41 @@ let parse_char = function
   | '/'     -> Some SLASH
   | _       -> None
 
-let is_digit = function
-| '0'..'9' -> true
-| _ -> false
-
-let is_letter = function
-| 'a'..'z' -> true
-| _ -> false
-
 type lexer_state = {
   str : string;
   len : int;
   pos : int;
 }
 
-let change_pos state new_pos = { str = state.str; len = state.len; pos = new_pos }
+let addi_pos state n = { str = state.str; len = state.len; pos = state.pos + n }
 
 let init_state input = { str = input; len = String.length input; pos = 0 }
 
 let try_1 state =
   if state.pos = state.len then None else
   match parse_char state.str.[state.pos] with
-  | Some token -> Some (change_pos state (state.pos + 1), token)
+  | Some token -> Some (addi_pos state 1, token)
   | None -> None
 
 let try_2 state =
   if state.len - state.pos < 2 then None else
   match parse_two_chars (state.str.[state.pos], state.str.[state.pos + 1]) with
-  | Some token -> Some (change_pos state (state.pos + 2), token)
+  | Some token -> Some (addi_pos state 2, token)
   | None -> None
 
 let rec take_int state index =
-  if index = state.len
-    then change_pos state index, INT(String.sub state.str state.pos (index - state.pos) |> int_of_string)
+  (* contains updated state and token as lazy pair; cause otherwise program may crash *)
+  let result = lazy(addi_pos state (index - state.pos), INT(String.sub state.str state.pos (index - state.pos) |> int_of_string)) in
+  if index = state.len then result
     else match state.str.[index] with
     | '0'..'9' -> take_int state (index + 1)
     | 'a'..'z' -> raise Invalid_token
-    | _ -> change_pos state index, INT(String.sub state.str state.pos (index - state.pos) |> int_of_string)
+    | _ -> result
+
 
 let rec take_str state index =
-  if index = state.len
-    then
-      let new_state = change_pos state index in
+  let new_state = addi_pos state (index - state.pos) in
+  if index = state.len then
       let str = String.sub state.str state.pos (index - state.pos) in
       match token_of_keyword str with
       | Some token -> new_state, token
@@ -79,7 +72,6 @@ let rec take_str state index =
     | 'a'..'z' -> take_str state (index + 1)
     | '0'..'9' -> raise Invalid_token
     | _ ->
-      let new_state = change_pos state index in
       let str = String.sub state.str state.pos (index - state.pos) in
       match token_of_keyword str with
       | Some token ->
@@ -91,7 +83,7 @@ let rec take_str state index =
 let try_n state =
   if state.pos = state.len then None else
   match state.str.[state.pos] with
-  | '0'..'9' -> Some (take_int state state.pos)
+  | '0'..'9' -> Some (take_int state state.pos |> Lazy.force)
   | 'a'..'z' -> Some (take_str state state.pos)
   | _ -> None
 
@@ -99,9 +91,7 @@ let is_unsignificant state = String.contains " \n\r\t" (state.str.[state.pos])
 
 let rec parse_loop state acc =
   if state.pos = state.len then acc else
-
-  if (is_unsignificant state) then parse_loop (change_pos state (state.pos + 1)) acc else
-
+  if is_unsignificant state then parse_loop (addi_pos state 1) acc else
   match try_2 state with
   | Some (state, token) -> parse_loop state (token :: acc)
   | None ->

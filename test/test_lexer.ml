8e4bdc85382e08c3f9495fa72f5430afd6e7d1ac
psiblvdegod <artyombaly@list.ru>
2025-07-11 00:45:17 +0300

    fix tests according to changes

--- test/test_lexer.ml
+++ test/test_lexer.ml
@@ -6,6 +6,8 @@ let correct_input_1 = "while for do done var"
 
 let correct_input_2 = "== != <= >= := () + - * / ;"
 
+let correct_input_3 = "var a := while * for ;"
+
 let incorrect_input_1 = "_]?"
 
 let incorrect_input_2 = String.empty
@@ -19,6 +21,11 @@ let tokens_of_string_passes_2 () =
   let expected_result = [EQ; NEQ; LEQ; GEQ; COLONEQQ; LP; RP; PLUS; MINUS; STAR; SLASH; SEMICOLON] in
   let actual_result = tokens_of_string correct_input_2 in
   check bool ("tokens_of_string on: " ^ correct_input_2) (expected_result = actual_result) true
+
+let tokens_of_string_passes_3 () =
+  let expected_result = [VAR; ID "a"; COLONEQQ; WHILE; STAR; FOR; SEMICOLON] in
+  let actual_result = tokens_of_string correct_input_3 in
+  check bool ("tokens_of_string on: " ^ correct_input_3) (expected_result = actual_result) true
 
 let tokens_of_string_raises_1 () =
   try
@@ -40,6 +47,7 @@ let tests_to_pass =
   [
     ("tokens_of_string passes on: " ^ correct_input_1, `Quick, tokens_of_string_passes_1);
     ("tokens_of_string passes on: " ^ correct_input_2, `Quick, tokens_of_string_passes_2);
+    ("tokens_of_string passes on: " ^ correct_input_3, `Quick, tokens_of_string_passes_3);
   ]
 
 let tests_to_raise =

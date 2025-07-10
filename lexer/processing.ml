444f5044654fe478b1805b41dc5a8bd51f7abd52
psiblvdegod <artyombaly@list.ru>
2025-07-11 00:01:38 +0300

    minor style changes

--- lexer/processing.ml
+++ lexer/processing.ml
@@ -10,11 +10,11 @@ let token_of_keyword = function
   | _       -> None
 
 let parse_two_chars = function
-  | "<="    -> Some LEQ 
-  | ">="    -> Some GEQ 
-  | "!="    -> Some NEQ 
-  | "=="    -> Some EQ 
-  | ":="    -> Some COLONEQQ
+  | '<', '='    -> Some LEQ 
+  | '>', '='    -> Some GEQ 
+  | '!', '='    -> Some NEQ 
+  | '=', '='    -> Some EQ 
+  | ':', '='    -> Some COLONEQQ
   | _       -> None
 
 let parse_char = function
@@ -29,9 +29,13 @@ let parse_char = function
   | '/'     -> Some SLASH
   | _       -> None
 
-let is_digit c = (Char.code '0' <= Char.code c) && (Char.code c <= Char.code '9')
+let is_digit = function
+| '0'..'9' -> true
+| _ -> false
 
-let is_letter c = (Char.code 'a' <= Char.code c) && (Char.code c <= Char.code 'z')
+let is_letter = function
+| 'a'..'z' -> true
+| _ -> false
 
 type lexer_state = {
   str : string;
@@ -51,29 +55,25 @@ let try_1 state =
 
 let try_2 state =
   if state.len - state.pos < 2 then None else
-  match parse_two_chars (String.sub state.str state.pos 2) with
+  match parse_two_chars (state.str.[state.pos], state.str.[state.pos + 1]) with
   | Some token -> Some (change_pos state (state.pos + 2), token)
   | None -> None
 
 let rec take_int state index =
   if index = state.len || (is_digit state.str.[index] = false)
-    then index
+    then change_pos state index, INT(String.sub state.str state.pos (index - state.pos) |> int_of_string)
     else take_int state (index + 1)
 
 let rec take_id state index =
   if index = state.len || (is_letter state.str.[index] = false)
-    then index
+    then change_pos state index, ID(String.sub state.str state.pos (index - state.pos))
     else take_id state (index + 1)
 
 let try_n state =
   if state.pos = state.len then None else
   match state.str.[state.pos] with
-  | x when is_digit x ->
-    let index = take_int state (state.pos) in
-    Some (change_pos state index, INT(String.sub state.str state.pos (index - state.pos) |> int_of_string))
-  | x when is_letter x ->
-    let index = take_id state (state.pos) in
-    Some (change_pos state index, ID(String.sub state.str state.pos (index - state.pos)))
+  | '0'..'9' -> Some (take_int state state.pos)
+  | 'a'..'z' -> Some (take_id state state.pos)
   | _ -> None
 
 let rec parse_loop state acc =

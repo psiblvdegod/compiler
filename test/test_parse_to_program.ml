78a7f21981898dc123a2fee1c6f9cdd4321d5578
psiblvdegod <artyombaly@list.ru>
2025-07-12 03:51:56 +0300

    move test for ite in separated module

--- test/test_parse_to_program.ml
+++ test/test_parse_to_program.ml
@@ -122,42 +122,9 @@ let tests_on_while =
     ("parse_to_program raises on:\n" ^ incorrect_input_while_2, `Quick, on_while_raises_2);
   ]
 
-let correct_input_ite_1 = "if a == 0 then a := 1; fi"
-
-let correct_input_ite_2 = "if a != a then var a b c; else var abc; fi"
-
-let correct_input_ite_3 = "
-while a != 0 do
-  if a < 0 then a := a + 1; else a := a - 1; fi
-done
-"
-
-let on_ite_passes_1 () =
-  let expected_result = [Ite(Eq(Var "a", Int 0), [Assignment("a", Int 1)], [])] in
-  let actual_result = tokenize correct_input_ite_1 |> parse_to_program in
-  check bool ("parse_to_program on: " ^ correct_input_ite_1) (actual_result = expected_result) true
-
-let on_ite_passes_2 () =
-  let expected_result = [Ite(Neq(Var "a", Var "a"), [Declaration(["a"; "b"; "c"])], [Declaration(["abc"])])] in
-  let actual_result = tokenize correct_input_ite_2 |> parse_to_program in
-  check bool ("parse_to_program on: " ^ correct_input_ite_2) (actual_result = expected_result) true
-
-let on_ite_passes_3 () =
-  let expected_result =
-    [While(Neq(Var "a", Int 0), [Ite(Lt(Var "a", Int 0), [Assignment("a", Add(Var "a", Int 1))], [Assignment("a", Sub(Var "a", Int 1))])])] in
-  let actual_result = tokenize correct_input_ite_3 |> parse_to_program in
-  check bool ("parse_to_program on: " ^ correct_input_ite_3) (actual_result = expected_result) true
-
-let tests_on_ite =
-  [
-    ("parse_to_program passes on:\n" ^ correct_input_ite_1, `Quick, on_ite_passes_1);
-    ("parse_to_program passes on:\n" ^ correct_input_ite_2, `Quick, on_ite_passes_2);
-    ("parse_to_program passes on:\n" ^ correct_input_ite_2, `Quick, on_ite_passes_3);
-  ]
 
 let () = run "test_parse_to_program.ml"
   [
     ("tests on assignment", tests_on_assignment);
     ("tests on while", tests_on_while);
-    ("tests on ite", tests_on_ite);
   ]

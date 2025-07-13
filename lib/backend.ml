d24adf3c6e8d5a511fcee3aa28d23d55a5b417ac
psiblvdegod <artyombaly@list.ru>
2025-07-13 04:09:19 +0300

    add simple version of assembly generation

--- lib/backend.ml
+++ lib/backend.ml
@@ -1,4 +1,5 @@
 open Types
+open Printf
 
 exception ReDeclaration_attempt
 exception Unbound_value
@@ -33,22 +34,42 @@ let index_of_var state name =
   | Some index -> index
 
 let assignment_const index value = 
-  Printf.sprintf "li t1, %d\nsw t1, %d(sp)\n" value (index * 16)
+  sprintf "li t1, %d\nsw t1, %d(sp)\n" value (index * 16)
 
 let lw_t1_by_index index =
-    Printf.sprintf "lw t1, %d(sp)\n" (index * 16)
+    sprintf "lw t1, %d(sp)\n" (index * 16)
 
 let sw_t1_by_index index =
-    Printf.sprintf "sw t1, %d(sp)\n" (index * 16)
+    sprintf "sw t1, %d(sp)\n" (index * 16)
+
+let push value =
+    sprintf
+"addi sp, sp, -16
+li t1, %d
+sw t1, 0(sp)\n" value
+
+let rec parse_expression state cnt = function
+  | Int value -> push value, cnt + 1
+  | Var name ->
+    let index = index_of_var state name in
+    lw_t1_by_index (index + cnt) ^ "addi sp, sp, -16\n" ^ "sw t1, 0(sp)\n", cnt + 1
+  | Add(left, right) ->
+    let left_str, left_cnt = parse_expression state cnt left in
+    let right_str, right_cnt = parse_expression state left_cnt right in
+    let lw_left = sprintf "lw t1, %d(sp)\n" (16 * (right_cnt - left_cnt)) in
+    let lw_right = "lw t2, 0(sp)\n" in
+    let sum = "add t1, t1, t2\n" in
+    let wipe_stack = sprintf "addi sp, sp, %d\n" (16 * (right_cnt - cnt - 1)) in
+    let push = "sw t1, 0(sp)\n" in
+    left_str ^ right_str ^ lw_left ^ lw_right ^ sum ^ wipe_stack ^ push, right_cnt - cnt
+  | _ -> raise Unsupported
 
 let process_assignment state dest expression =
   let dest_index = index_of_var state dest in
-  let get_str = function
-  | Int value -> assignment_const dest_index value
-  | Var source ->
-    (index_of_var state source |> lw_t1_by_index) ^ (sw_t1_by_index dest_index)
-  | _ -> raise Unsupported in
-  append_to_acc state (get_str expression)
+  let expr, _ = parse_expression state 0 expression in
+  (* if rest != 1 then failwith ";(" else *)
+  expr ^ "lw t1, 0(sp)\n" ^ "addi sp, sp, 16\n" ^ (sprintf "sw t1, %d(sp)\n" (16 * dest_index))
+  |> append_to_acc state
 
 let rec assembly_of_program state = function
   | [] -> state.acc
@@ -70,9 +91,16 @@ let init_state =
 "
 .global _start
 .section .text
-    
+
 _start:
 "
   }
 
-let assembly_of_program program = assembly_of_program init_state program
+let exit_code =
+"
+li a7, 93
+li a0, 0
+ecall
+"
+
+let assembly_of_program program = assembly_of_program init_state program ^ exit_code
--- test/backend/assignment.ml
+++ test/backend/assignment.ml
@@ -3,7 +3,7 @@ open Compiler.Types
 open Compiler.Backend
 
 let%expect_test "assignment test" =
-  let program = [Declaration(["a"]);Assignment("a", Int 5)] in
+  let program = [Declaration(["a"]); Assignment("a", Int 5)] in
   assembly_of_program program |> printf "%s";
   [%expect {|
     .global _start
@@ -11,8 +11,16 @@ let%expect_test "assignment test" =
 
     _start:
     addi sp, sp, -16
+    addi sp, sp, -16
     li t1, 5
     sw t1, 0(sp)
+    lw t1, 0(sp)
+    addi sp, sp, 16
+    sw t1, 0(sp)
+
+    li a7, 93
+    li a0, 0
+    ecall
     |}]
 
 let%expect_test "assignment test" =
@@ -26,8 +34,86 @@ let%expect_test "assignment test" =
     addi sp, sp, -16
     addi sp, sp, -16
     addi sp, sp, -16
+    addi sp, sp, -16
     li t1, 5
+    sw t1, 0(sp)
+    lw t1, 0(sp)
+    addi sp, sp, 16
     sw t1, 32(sp)
     lw t1, 32(sp)
+    addi sp, sp, -16
+    sw t1, 0(sp)
+    lw t1, 0(sp)
+    addi sp, sp, 16
     sw t1, 16(sp)
+
+    li a7, 93
+    li a0, 0
+    ecall
+    |}]
+
+let%expect_test "addition test" =
+  let program = [Declaration(["a"]); Assignment("a", Add(Int 10, Int 20))] in
+  assembly_of_program program |> printf "%s";
+  [%expect {|
+    .global _start
+    .section .text
+
+    _start:
+    addi sp, sp, -16
+    addi sp, sp, -16
+    li t1, 10
+    sw t1, 0(sp)
+    addi sp, sp, -16
+    li t1, 20
+    sw t1, 0(sp)
+    lw t1, 16(sp)
+    lw t2, 0(sp)
+    add t1, t1, t2
+    addi sp, sp, 16
+    sw t1, 0(sp)
+    lw t1, 0(sp)
+    addi sp, sp, 16
+    sw t1, 0(sp)
+
+    li a7, 93
+    li a0, 0
+    ecall
+    |}]
+
+let%expect_test "addition test" =
+  let program = [Declaration(["a"]); Assignment("a", Add(Int 10, Add(Var "a", Int 20)))] in
+  assembly_of_program program |> printf "%s";
+  [%expect {|
+    .global _start
+    .section .text
+
+    _start:
+    addi sp, sp, -16
+    addi sp, sp, -16
+    li t1, 10
+    sw t1, 0(sp)
+    lw t1, 16(sp)
+    addi sp, sp, -16
+    sw t1, 0(sp)
+    addi sp, sp, -16
+    li t1, 20
+    sw t1, 0(sp)
+    lw t1, 16(sp)
+    lw t2, 0(sp)
+    add t1, t1, t2
+    addi sp, sp, 16
+    sw t1, 0(sp)
+    lw t1, 16(sp)
+    lw t2, 0(sp)
+    add t1, t1, t2
+    addi sp, sp, 16
+    sw t1, 0(sp)
+    lw t1, 0(sp)
+    addi sp, sp, 16
+    sw t1, 0(sp)
+
+    li a7, 93
+    li a0, 0
+    ecall
     |}]

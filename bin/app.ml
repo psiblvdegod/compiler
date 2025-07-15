3b76172f6a9f5ee86eaeb0873bdbf1d4e4815b29
psiblvdegod <artyombaly@list.ru>
2025-07-16 01:14:27 +0300

    minor project structure changes

--- bin/app.ml
+++ bin/app.ml
@@ -21,13 +21,13 @@ let _exit_code =
 call exit
 "
 
-let compile str =
+let str_to_assembly str =
   _start_code ^ (str |> tokenize |> parse_to_program |> assembly_of_program) ^ _exit_code
 
 let () =
   let input_channel = Sys.argv.(1) |> open_in in
   let source = in_channel_length input_channel |> really_input_string input_channel in
   let output_channel = Sys.argv.(2) |> open_out in
-  compile source |> output_string output_channel;
+  str_to_assembly source |> output_string output_channel;
   close_in input_channel;
   close_out output_channel;
---
+++ run.sh
@@ -0,0 +1,33 @@
+if [ "$#" -ne 2 ]; then
+    echo "error: 2 arguments expected"
+    echo "hint: run.sh <source.lang> <destination.exe>"
+    exit 1
+fi
+
+compiler="./_build/default/bin/app.exe"
+
+if [ ! -f $compiler ]; then
+    dune clean
+    dune build
+
+    if [ ! -f $compiler ]; then
+        echo "error: compiler not found"
+        echo "hint: cd to repo root dir"
+        exit 2
+    fi
+fi
+
+stdlib="./lib/stdlib.s"
+
+if [ ! -f $stdlib ]; then
+    echo "error: stdlib.s not found"
+    echo "hint: cd to repo root dir"
+    exit 2
+fi
+
+dune exec $compiler "$1" program.s
+riscv64-unknown-elf-as program.s -o program.o
+riscv64-unknown-elf-as $stdlib -o stdlib.o
+riscv64-unknown-elf-ld program.o stdlib.o -o "$2"
+
+rm ./program.s ./program.o ./stdlib.o

c60524ff7488ae6f5c5772f3dca5e90978d0cd48
psiblvdegod <artyombaly@list.ru>
2025-07-16 00:31:16 +0300

    remove print_stack

--- bin/app.ml
+++ bin/app.ml
@@ -8,7 +8,6 @@ let _start_code =
 .global _start
 
 _start:
-call save_sp
 
 # generated code
 # =======================================
@@ -19,17 +18,16 @@ let _exit_code =
 "
 # =======================================
 
-call print_stack
 call exit
 "
 
-let assembly_of_string str =
+let compile str =
   _start_code ^ (str |> tokenize |> parse_to_program |> assembly_of_program) ^ _exit_code
 
 let () =
   let input_channel = Sys.argv.(1) |> open_in in
   let source = in_channel_length input_channel |> really_input_string input_channel in
   let output_channel = Sys.argv.(2) |> open_out in
-  assembly_of_string source |> output_string output_channel;
+  compile source |> output_string output_channel;
   close_in input_channel;
   close_out output_channel;

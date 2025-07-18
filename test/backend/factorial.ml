a55c50b9736626a226f4c3a84beb13d6f023509d
psiblvdegod <artyombaly@list.ru>
2025-07-18 03:54:13 +0300

    add some tests

--- test/backend/factorial.ml
+++ test/backend/factorial.ml
@@ -1,9 +1,9 @@
 open Printf
 open Compiler.App
 
-let rr = Sys.getenv "REPO_ROOT"
+let repo_root = Sys.getenv "REPO_ROOT"
 
-let stdlib = sprintf "%s/lib/stdlib.s" rr
+let stdlib = sprintf "%s/lib/stdlib.s" repo_root
 
 let run input =
     let src = "./program.lang" in
@@ -16,14 +16,14 @@ let run input =
     compile ic oc;
     close_in ic;
     close_out oc;
-    Sys.command @@ sprintf "riscv64-unknown-elf-as %s -o ./program.o" dest |> ignore;
-    Sys.command @@ sprintf "riscv64-unknown-elf-as %s -o ./stdlib.o" stdlib |> ignore;
+    Sys.command @@ sprintf "riscv64-unknown-elf-as %s -o program.o" dest |> ignore;
+    Sys.command @@ sprintf "riscv64-unknown-elf-as %s -o stdlib.o" stdlib |> ignore;
     Sys.command @@ sprintf "riscv64-unknown-elf-ld program.o stdlib.o -o program" |> ignore;
     Sys.command @@ sprintf "spike pk program" |> ignore;
 
 ;;
 
-let factorial_input =
+let factorial =
 "
 var n acc;
 
@@ -39,7 +39,110 @@ print acc;
 "
 
 let%expect_test "factorial" =
-  run factorial_input;
+  run factorial;
   [%expect {| 5040 |}];
+
+;;
+
+let fibonacci =
+"
+var a b n;
+
+n := 7;
+
+a := 0;
+b := 1;
+
+while n != 0 do
+    b := b + a;
+    a := b - a;
+    n := n - 1;
+done
+
+print b;
+"
+
+let%expect_test "fibonacci" =
+  run fibonacci;
+  [%expect {| 21 |}];
+
+;;
+
+let xor =
+"
+var a b xor;
+
+a := 0;
+b := 1;
+
+if a == 0 then
+    if b == 0 then
+        xor := 0;
+    else
+        xor := 1;
+    fi
+else
+    if b == 0 then
+        xor := 1;
+    else
+        xor := 0;
+    fi
+fi
+
+print xor;
+"
+
+let%expect_test "xor" =
+  run xor;
+  [%expect {| 1 |}];
+
+;;
+
+let bubble_sort_on_4 =
+"
+var a b c d issorted tmp;
+
+a := 2;
+b := 3;
+c := 1;
+d := 0;
+
+issorted := 0;
+
+while issorted != 1 do
+    issorted := 1;  
+    if a > b then
+        tmp := a;
+        a := b;
+        b := tmp;
+        issorted := 0;
+    fi
+
+    if b > c then
+        tmp := b;
+        b := c;
+        c := tmp;
+        issorted := 0;
+    fi
+
+    if c > d then
+        tmp := c;
+        c := d;
+        d := tmp;
+        issorted := 0;
+    fi
+done
+
+print a b c d;
+"
+
+let%expect_test "bubble sort" =
+  run bubble_sort_on_4;
+  [%expect {|
+    0
+    1
+    2
+    3
+    |}];
 
 ;;

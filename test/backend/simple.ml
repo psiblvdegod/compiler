e64513409eb83f570ea5e9c80bebf455623630e2
psiblvdegod <artyombaly@list.ru>
2025-07-18 04:59:24 +0300

    rename test module

--- test/backend/simple.ml
+++ test/backend/simple.ml
@@ -1,27 +1,4 @@
-open Printf
 open Compiler.App
-
-let repo_root = Sys.getenv "REPO_ROOT"
-
-let stdlib = sprintf "%s/lib/stdlib.s" repo_root
-
-let run input =
-    let src = "./program.lang" in
-    let dest = "./program.s" in
-    let oc = open_out src in
-    output_string oc input;
-    close_out oc;
-    let ic = open_in src in
-    let oc = open_out dest in
-    compile ic oc;
-    close_in ic;
-    close_out oc;
-    Sys.command @@ sprintf "riscv64-unknown-elf-as %s -o program.o" dest |> ignore;
-    Sys.command @@ sprintf "riscv64-unknown-elf-as %s -o stdlib.o" stdlib |> ignore;
-    Sys.command @@ sprintf "riscv64-unknown-elf-ld program.o stdlib.o -o program" |> ignore;
-    Sys.command @@ sprintf "spike pk program" |> ignore;
-
-;;
 
 let factorial =
 "

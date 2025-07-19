398dd21f863af6b9baba505df856342a97420ac1
psiblvdegod <artyombaly@list.ru>
2025-07-20 02:43:53 +0300

    minor fixes in app.ml

--- lib/app.ml
+++ lib/app.ml
@@ -3,6 +3,8 @@ open Parser
 open Backend
 open Printf
 
+exception EnvVarIsNotSpecified of string
+
 let _start_code =
 "
 .global _start
@@ -18,29 +20,30 @@ let _exit_code =
 call exit
 "
 
-let compile text =
-    let generated_code = text |> tokenize |> parse_to_program |> assembly_of_program in
-    _start_code ^ generated_code ^ _exit_code
+let clear = lazy (Sys.command "rm program program.o stdlib.o program.s" |> ignore)
 
 let clear_on_failure = function
     | 0 -> ()
     | _ ->
-        (Sys.command "rm program program.o stdlib.o program.s program.lang" |> ignore;
-        failwith "something went wrong")
-
-;;
+        (clear |> Lazy.force;
+        failwith "something went wrong";)
 
 let repo_root =
     match Sys.getenv_opt "REPO_ROOT" with
     | Some value -> value
-    | None -> failwith @@ "export REPO_ROOT\n" ^ "example: ~/Desktop/compiler"
+    | None -> raise (EnvVarIsNotSpecified "REPO_ROOT")
 
 let emulator =
     match Sys.getenv_opt "EMULATOR" with
     | Some value -> value
-    | None -> failwith "export EMULATOR\n" ^ "\"spike pk\" or \"qemu-riscv64\" or something else"
+    | None -> raise (EnvVarIsNotSpecified "EMULATOR")
 
-let stdlib = sprintf "%s/lib/stdlib.s" repo_root
+let stdlib = repo_root ^ "/lib/stdlib.s"
+
+let compile text =
+    let generated_code = text |> tokenize |> parse_to_program |> assembly_of_program in
+    
+    _start_code ^ generated_code ^ _exit_code
 
 let save_as text path =
     let oc = open_out path in
@@ -56,5 +59,6 @@ let run input =
     Sys.command @@ sprintf "riscv64-unknown-elf-as %s -o stdlib.o" stdlib |> clear_on_failure;
     Sys.command @@ sprintf "riscv64-unknown-elf-ld program.o stdlib.o -o program" |> clear_on_failure;
     Sys.command @@ sprintf "%s program" emulator |> clear_on_failure;
+    clear |> Lazy.force;
 
 ;;

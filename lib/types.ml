040e25c6f0ea97d5a3f8020836fec988e73a9806
psiblvdegod <artyombaly@list.ru>
2025-07-21 05:03:41 +0300

    expand type system

--- lib/types.ml
+++ lib/types.ml
@@ -1,37 +1,53 @@
 exception Invalid_statement
 exception Invalid_expression
 
-type expression =
+type expr =
+  | Int_Expr of int_expr
+  | Bool_Expr of bool_expr
+  | Str_Expr of str_expr
+
+and int_expr =
   | Int of int
   | Var of string
-  | Neg of expression
-  | BinOp of binary_operation * expression * expression
-  | Call of string * expression list
+  | Neg of int_expr
+  | BinOp of int_binop * int_expr * int_expr
 
-and binary_operation =
+and int_binop =
   | Add
   | Sub
   | Div
   | Mul
 
-and program = statement list
+and bool_expr =
+  | True
+  | False
+  | Var of string
+  | IntCmp of int_cmp * int_expr * int_expr
+  | StrCmp of str_expr * str_expr
 
-and statement =
-  | Declaration of string list
-  | Assignment of string * expression
-  | While of boolean_expression * program
-  | Ite of boolean_expression * program * program
-  | Call of string * expression list
-
-and boolean_expression =
-  | true
-  | false
-  | Comparison of comparison * expression * expression
-
-and comparison =
+and int_cmp =
   | Eq
   | Neq
   | Lt
   | Gt
   | Leq
   | Geq
+
+and str_expr =
+  | Str of string
+  | Var of string
+  | StrOp of str_op
+
+and str_op =
+  | Rev of str_expr
+  | Cat of str_expr list
+
+and statement =
+  | Declaration of string list
+  | Definition of string * string list * program
+  | Assignment of string * expr
+  | While of bool_expr * program
+  | Ite of bool_expr * program * program
+  | Call of string * expr list
+
+and program = statement list

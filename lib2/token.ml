8b227247a5fbfa4dcdfb07508affa0e1c0f0f599
psiblvdegod <artyombaly@list.ru>
2025-07-22 03:14:28 +0300

    temporarily copy lexer files (for the new version of the parser)

---
+++ lib2/token.ml
@@ -0,0 +1,29 @@
+exception Invalid_token
+
+type token =
+| ID of string
+| INT of int
+| TRUE
+| FALSE
+| WHILE
+| DO
+| DONE
+| VAR
+| IF
+| THEN
+| ELSE
+| FI
+| PLUS
+| MINUS
+| STAR
+| SLASH
+| EQ
+| NEQ
+| COLONEQQ
+| LEQ
+| GEQ
+| LT
+| GT
+| LP
+| RP
+| SEMICOLON

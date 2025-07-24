66f2e2048584c45bf9a5351aac6c659107e71885
psiblvdegod <artyombaly@list.ru>
2025-07-25 01:48:45 +0300

    add tests for type checker

--- test/inferencer/type_checker.ml
+++ test/inferencer/type_checker.ml
@@ -14,10 +14,37 @@ e := b < a;
 f := e;
 "
 
+let test2 =
+"
+var a b c;
+
+a := \"zxc\";
+
+b := a;
+
+"
+
 let type_check str = str |> tokenize |> parse_to_program |> pp_types_of_program
 
 let%expect_test "test1" =
   type_check test1;
-  [%expect {| 5040 |}];
+  [%expect {|
+    Name: a | Type: Integer
+    Name: b | Type: Integer
+    Name: c | Type: Integer
+    Name: d | Type: Integer
+    Name: e | Type: Boolean
+    Name: f | Type: Boolean
+    |}];
+
+;;
+
+let%expect_test "test2" =
+  type_check test2;
+  [%expect {|
+    Name: c | Type: Not specified
+    Name: a | Type: String
+    Name: b | Type: String
+    |}];
 
 ;;

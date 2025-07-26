02e6b1c5a0b3ee9e549afb2f6308286a178390d0
psiblvdegod <artyombaly@list.ru>
2025-07-26 19:28:36 +0300

    move type token to types.ml

--- lib/types.ml
+++ lib/types.ml
@@ -1,3 +1,43 @@
+(* lexer *)
+
+type lexer_error =
+| Invalid_token
+| Input_is_empty
+[@@deriving show { with_path = false }]
+
+type token =
+| ID of string
+| INT of int
+| TRUE
+| FALSE
+| STR of string
+| WHILE
+| DO
+| DONE
+| VAR
+| IF
+| THEN
+| ELSE
+| FI
+| PLUS
+| MINUS
+| STAR
+| SLASH
+| CAT
+| AND
+| OR
+| EQ
+| NEQ
+| COLONEQQ
+| LEQ
+| GEQ
+| LT
+| GT
+| LP
+| RP
+| SEMICOLON
+[@@deriving show { with_path = false }]
+
 (* parser *)
 
 type parser_error =

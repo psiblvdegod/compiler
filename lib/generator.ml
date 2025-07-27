b658b4b6c78092e69b55732cde23fffb64760bd6
psiblvdegod <artyombaly@list.ru>
2025-07-27 23:53:24 +0300

    add compile_call and print in generator.ml

--- lib/generator.ml
+++ lib/generator.ml
@@ -12,6 +12,7 @@ let find_var_index scope name =
 let sp = "sp"
 let t1 = "t1"
 let t2 = "t2"
+let a0 = "a0"
 
 let addi dest src lit = [sprintf "addi %s, %s, %d\n" dest src lit ]
 let li reg lit = [sprintf "li %s, %d\n" reg lit]
@@ -24,6 +25,8 @@ let push reg = addi sp sp (-alignment) @ [sprintf "sw %s, (sp)\n" reg]
 let pop reg = [sprintf "lw %s, (sp)\n" reg] @ addi sp sp alignment
 let lw_from_stack reg delta = [sprintf "lw %s, %d(sp)\n" reg delta]
 let sw_to_stack reg delta = [sprintf "sw %s, %d(sp)\n" reg delta]
+let call f = [sprintf "call %s\n" f]
+let print_number = "print_number"
 
 let apply_binop = function
   | Add -> add t1 t1 t2
@@ -95,13 +98,40 @@ let rec compile_program typed_program acc =
       let acc = acc
       @ addi sp sp delta
     in compile_program rest acc
-    | Typed_Assignment (name, typed_expr) ->
+    | Typed_Assignment(name, typed_expr) ->
       let pos = find_var_index scope name * alignment in
       let acc = acc
       @ compile_expressions scope [typed_expr]
       @ pop t1
       @ sw_to_stack t1 pos
       in compile_program rest acc
+
+
+
+    | Typed_Call(name, args) ->
+      let acc = acc @ (compile_call scope name args) in
+      compile_program rest acc
     | _ -> failwith "not implemented"
+
+(* stdlib *)
+
+and compile_call scope name args =
+  match name with
+  | "print" -> ll_print scope args []
+  | _ -> failwith "not implemented"
+
+and ll_print scope exprs acc =
+  match exprs with
+  | [] -> acc
+  | expr :: rest ->
+    let acc = acc @ compile_expressions scope [expr] in
+    let acc = 
+    match expr with
+    | Type_Int _ ->
+      acc
+      @ pop a0
+      @ call print_number
+    | _ -> failwith "not implemented"
+    in ll_print scope rest acc
 
 let assembly_of_typed_program typed_program = String.concat String.empty (compile_program typed_program [])

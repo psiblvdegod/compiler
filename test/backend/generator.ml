a1a6fbdf58b3c953838aebee805be4fefaf17c54
psiblvdegod <artyombaly@list.ru>
2025-07-27 22:35:22 +0300

    add test for generator

--- test/backend/generator.ml
+++ test/backend/generator.ml
@@ -17,22 +17,15 @@ let compile str =
 
 let test1 =
 "
-var a b c;
+var a b;
 a := 1;
-b := a;
 "
 
 let%expect_test "test1" =
   compile test1;
   [%expect {|
-    addi sp, sp, -96
+    addi sp, sp, -64
     li t1, 1
-    addi sp, sp, -32
-    sw t1, (sp)
-    lw t1, (sp)
-    addi sp, sp, 32
-    sw t1, 64(sp)
-    lw t1, 64(sp)
     addi sp, sp, -32
     sw t1, (sp)
     lw t1, (sp)
@@ -42,29 +35,70 @@ let%expect_test "test1" =
 
 ;;
 
-
 let test2 =
 "
 var a;
-a := 5 + 2;
+a := 1 + 5;
 "
-
 let%expect_test "test2" =
   compile test2;
   [%expect {|
     addi sp, sp, -32
-    li t1, 5
+    li t1, 1
     addi sp, sp, -32
     sw t1, (sp)
-    li t1, 2
+    li t1, 5
     addi sp, sp, -32
     sw t1, (sp)
     lw t1, 32(sp)
     lw t2, 0(sp)
-    addi sp, sp, 64
     add t1, t1, t2
+    addi sp, sp, 32
+    sw t1, 0(sp)
+    lw t1, (sp)
+    addi sp, sp, 32
+    sw t1, 0(sp)
+    |}];
+
+;;
+
+
+let test1 =
+"
+var a;
+a := 1;
+a := 10 + 5 * a;
+
+"
+let%expect_test "test1" =
+  compile test1;
+  [%expect {|
+    addi sp, sp, -32
+    li t1, 1
     addi sp, sp, -32
     sw t1, (sp)
+    lw t1, (sp)
+    addi sp, sp, 32
+    sw t1, 0(sp)
+    li t1, 10
+    addi sp, sp, -32
+    sw t1, (sp)
+    li t1, 5
+    addi sp, sp, -32
+    sw t1, (sp)
+    lw t1, 64(sp)
+    addi sp, sp, -32
+    sw t1, (sp)
+    lw t1, 32(sp)
+    lw t2, 0(sp)
+    mul t1, t1, t2
+    addi sp, sp, 32
+    sw t1, 0(sp)
+    lw t1, 32(sp)
+    lw t2, 0(sp)
+    add t1, t1, t2
+    addi sp, sp, 32
+    sw t1, 0(sp)
     lw t1, (sp)
     addi sp, sp, 32
     sw t1, 0(sp)
